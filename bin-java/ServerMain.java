69dd24e7ad532c0ff822cce6b588121f94069a13
T. Alexander <t.alex@outlook.com>
2014-01-08 22:36:46 -0500

    Update ServerMain.java
    Added some more framework for the three time periods of eco-mode. Fixed bugs with tracking how much time left, and the progress bar works, but something is going wrong on times too close to the current and/or times that involve half hours.

--- bin-java/ServerMain.java
+++ bin-java/ServerMain.java
@@ -1,8 +1,8 @@
 public class ServerMain {
+	public static int EcoModeSegment = 0;
 	public static void main(String args[]) {
 		boolean TextsEnabled = false; //TODO enable text message notifications.
-		double TimeUntilMaint, TimeUntilOn, TimeUntilOff, MinutesSection, RealTimeAway = 0;
-		String ActionSentence = "";
+		double TimeUntilMaint = 0, TimeUntilOn = 0, TimeUntilOff = 0;
 		ServerLoad x = new ServerLoad();
 		x.pack();
 		x.setVisible(true);
@@ -27,7 +27,7 @@ public class ServerMain {
 		y.SetCAProgress(0, "Reading Default Eco-Mode Schedule...");
 		TimeSchedule.ReadEcoTimes();
 		y.SetCAProgress(0, "Sending Startup Message...");
-		if (TextsEnabled) SendMail.Email("2405666903@vtext.com", "Server has started under the " + ServerLoad.ServerName + " profile. " + TimeSchedule.TimeStamp());
+		if (TextsEnabled) SendMail.Email("LeLeLePhone@vtext.com", "Server has started under the " + ServerLoad.ServerName + " profile. " + TimeSchedule.TimeStamp());
 		y.CurrentActivityIndef(false);
 		y.SetCAProgress(0, "...");
 		y.SetTimeProgress(100, "∞ until Power Off");
@@ -36,6 +36,7 @@ public class ServerMain {
 			if (OSD.ActionButtonPressed == "A") System.exit(0); //TODO make it close all open windows. Autohotkey Scripts.
 			if (OSD.ActionButtonPressed == "C" && TimeSchedule.EcoMode) {
 				TimeSchedule.EcoMode = false;
+				EcoModeSegment = 0;
 				OSD.ActionButtonPressed = ""; }
 			else if (OSD.ActionButtonPressed == "C") {
 				OSD.ActionButtonPressed = "";
@@ -48,34 +49,37 @@ public class ServerMain {
 			y.SetCAProgress((int) Math.round(100 - (UpdateCountdown * 100) / 600), (UpdateCountdown / 2) + " seconds until update.");
 	//Countdown to Next Major Action
 			if (TimeSchedule.EcoMode) {
-				TimeUntilMaint = (TimeSchedule.MaintTime / 100) - TimeSchedule.CurrentHour();
-				TimeUntilOn = (TimeSchedule.ServerOn / 100) - TimeSchedule.CurrentHour();
-				TimeUntilOff = (TimeSchedule.ServerOff / 100) - TimeSchedule.CurrentHour();
-				MinutesSection = TimeSchedule.CurrentMinute(); MinutesSection = MinutesSection / 60;
-				if (TimeUntilMaint < 0) TimeUntilMaint = 24 - TimeSchedule.CurrentHour() + (TimeSchedule.MaintTime / 100);
-				if (TimeUntilOn < 0) TimeUntilOn = 24 - TimeSchedule.CurrentHour() + (TimeSchedule.ServerOn / 100);
-				if (TimeUntilOff < 0) TimeUntilOff = 24 - TimeSchedule.CurrentHour() + (TimeSchedule.ServerOff / 100);
-				if (TimeUntilMaint < TimeUntilOn && TimeUntilMaint < TimeUntilOff) {
-					RealTimeAway = TimeUntilMaint - 1 + (1 - MinutesSection);
-					ActionSentence = "Maintenance"; }
-				else if (TimeUntilOn < TimeUntilMaint && TimeUntilOn < TimeUntilOff) {
-					RealTimeAway = TimeUntilOn - 1 + (1 - MinutesSection);
-					ActionSentence = "Server On"; }
-				else if (TimeUntilOff < TimeUntilMaint && TimeUntilOff < TimeUntilOn) {
-					RealTimeAway = TimeUntilOff - 1 + (1 - MinutesSection);
-					ActionSentence = "Server Off"; }
-				y.SetTimeProgress(10, ((int) Math.floor(RealTimeAway)) + " hrs " + (int) Math.round(((RealTimeAway - Math.floor(RealTimeAway)) * 60)) + " min until " + ActionSentence); }
+				y.SetTimeProgress(100, "Congratulations, You Found A Bug");
+				TimeUntilOn = TimeSchedule.TimeUntil(TimeSchedule.ServerOn);
+				TimeUntilOff = TimeSchedule.TimeUntil(TimeSchedule.ServerOff);
+				TimeUntilMaint = TimeSchedule.TimeUntil(TimeSchedule.MaintTime);
+				if (TimeUntilOn < TimeUntilOff && TimeUntilOn < TimeUntilMaint && TimeUntilOn > 0) {
+					y.SetTimeProgress(100 - ((int) Math.round(100 * (TimeUntilOn / TimeSchedule.TimeBetween(TimeSchedule.MaintTime, TimeSchedule.ServerOn)))), (int) Math.floor(TimeUntilOn) + " hrs " + (int) Math.round((TimeUntilOn - Math.floor(TimeUntilOn)) * 60) + " min until Server On.");
+					EcoModeSegment = 1; }
+				if (TimeUntilOff < TimeUntilOn && TimeUntilOff < TimeUntilMaint && TimeUntilOff > 0) {
+					y.SetTimeProgress(100 - ((int) Math.round(100 * (TimeUntilOff / TimeSchedule.TimeBetween(TimeSchedule.ServerOn, TimeSchedule.ServerOff)))), (int) Math.floor(TimeUntilOff) + " hrs " + (int) Math.round((TimeUntilOff - Math.floor(TimeUntilOff)) * 60) + " min until Server Off.");
+					EcoModeSegment = 2; }
+				if (TimeUntilMaint < TimeUntilOn && TimeUntilMaint < TimeUntilOff && TimeUntilMaint > 0) {
+					y.SetTimeProgress(100 - ((int) Math.round(100 * (TimeUntilMaint / TimeSchedule.TimeBetween(TimeSchedule.ServerOff, TimeSchedule.MaintTime)))), (int) Math.floor(TimeUntilMaint) + " hrs " + (int) Math.round((TimeUntilMaint - Math.floor(TimeUntilMaint)) * 60) + " min until Maintenance.");
+					EcoModeSegment = 3; } }
 			else y.SetTimeProgress(100, "∞ until Power Off");
-			y.Output("Current Time: " + TimeSchedule.CurrentMilitaryTime());
+			if (EcoModeSegment == 1) {
+				//do hibernation 1 min before server on.
+				//make sure maintenance stops before this code triggers.
+			}
+			if (EcoModeSegment == 2) {
+				//server is running until server off.
+				//if server not running initialize everything.
+			}
+			if (EcoModeSegment == 3) {
+				//hibernation until 1 min before maint.
+				//a long sleep.
+				//do the maintenance here?
+			}
+			y.Output("Current Segment for Eco: " + EcoModeSegment);
 			if (UpdateCountdown == 0) {
 				//TODO trigger update: check server status, cpu load/heat, players, network... AutoHotKey scripts...
 				UpdateCountdown = 600; }
 			try {Thread.sleep(500); }
 			catch (InterruptedException e) {e.printStackTrace(); }
-			UpdateCountdown--;
-		}
-		
-		
-	}
-
-}
+			UpdateCountdown--; } } }
