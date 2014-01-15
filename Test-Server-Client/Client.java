2f813d55dd2e3a3227b04288eac5828229a3dfbc
T. Alexander <t.alex@outlook.com>
2014-01-14 21:09:27 -0500

    Create Client.java
    Server Socket Client

---
+++ Test-Server-Client/Client.java
@@ -0,0 +1,13 @@
+import java.net.*;
+import java.io.*;
+public class Client {
+	static Socket socket;
+	static DataInputStream in;
+	public static void main(String args[]) throws Exception {
+		System.out.println("Connecting...");
+		socket = new Socket("localhost", 7777);
+		System.out.println("Connection established.");
+		in = new DataInputStream(socket.getInputStream());
+		System.out.println("Recieving Information...");
+		String test = in.readUTF();
+		System.out.println("Message from server: " + test); } }
