d595016cea56f3d069acb5e32648b47646d94825
T. Alexander <t.alex@outlook.com>
2014-01-14 21:06:29 -0500

    Update ServerMain.java
    Update: last update pre-server

--- bin-java/ServerMain.java
+++ bin-java/ServerMain.java
@@ -1,39 +1,50 @@
+import java.io.InputStream; import org.hyperic.sigar.shell.*;
 public class ServerMain {
 	public static int EcoModeSegment = 0;
+	public static boolean TextsEnabled = false;
 	public static void main(String args[]) {
-		boolean TextsEnabled = false; //TODO enable text message notifications.
-		double TimeUntilMaint = 0, TimeUntilOn = 0, TimeUntilOff = 0;
+	//CREATE PROFILES PANEL
 		ServerLoad x = new ServerLoad();
 		x.pack();
 		x.setVisible(true);
 		x.setResizable(false);
 		x.setSize(300, 300);
+	//WAIT FOR A CHOICE
 		while (!ServerLoad.ChoiceMade) {
 			try {Thread.sleep(200);}
 			catch (InterruptedException e) {e.printStackTrace();} }
 		x.dispose();
+	//INITIATE THE OSD
 		OSD y = new OSD();
 		y.pack();
 		y.setVisible(true);
 		y.setResizable(false);
 		y.setSize(400, 200);
+	//LAUNCH SERVER JAR
 		y.CurrentActivityIndef(true);
-		//y.SetCAProgress(0, "Launching Server..."); TODO FIX THIS PART making things runnable.
-		//try {@SuppressWarnings("unused")
-		//Process proc = Runtime.getRuntime().exec("java -jar " + ServerLoad.ServerLaunchPath); }
-		//catch (IOException e1) {
-		//	y.SetCAProgress(0, "Server launch failed. Check path.");
-		//	try {Thread.sleep(3000); } catch (InterruptedException e) {e.printStackTrace(); } }
+		y.SetCAProgress(0, "Launching Server...");
+		try { //Can't have spaces in file path. Thanks Obama.
+			Process proc = Runtime.getRuntime().exec("java -jar " + ServerLoad.ServerLaunchPath);
+			@SuppressWarnings("unused") InputStream in = proc.getInputStream();
+			@SuppressWarnings("unused") InputStream err = proc.getErrorStream(); }
+		catch (Exception e1) {y.SetCAProgress(0, "Server launch failed. Check path."); }
+	//READ ECO MODE TIMES
 		y.SetCAProgress(0, "Reading Default Eco-Mode Schedule...");
 		TimeSchedule.ReadEcoTimes();
+	//SEND STARTUP MESSAGE
 		y.SetCAProgress(0, "Sending Startup Message...");
-		if (TextsEnabled) SendMail.Email("LeLeLePhone@vtext.com", "Server has started under the " + ServerLoad.ServerName + " profile. " + TimeSchedule.TimeStamp());
+		if (TextsEnabled) SendMail.Email("LeLeLePhone@vtext.com", TimeSchedule.TimeStamp() + " Server has started under the *" + ServerLoad.ServerName + "* profile.");
 		y.CurrentActivityIndef(false);
-		y.SetCAProgress(0, "...");
-		y.SetTimeProgress(100, "∞ until Power Off");
+	//SET VARIABLES FOR RUNNING
 		int UpdateCountdown = 600;
+		double TimeUntilMaint = 0, TimeUntilOn = 0, TimeUntilOff = 0;
+	//END OF STARTUP PROCESS
 		while (true) {
 			if (OSD.ActionButtonPressed == "A") System.exit(0); //TODO make it close all open windows. Autohotkey Scripts.
+			if (OSD.ActionButtonPressed == "B") {
+				//TODO account for restart and changing running profile.
+				OSD.ActionButtonPressed = "";
+				System.out.println("LElelelelellle"); }
 			if (OSD.ActionButtonPressed == "C" && TimeSchedule.EcoMode) {
 				TimeSchedule.EcoMode = false;
 				EcoModeSegment = 0;
@@ -62,7 +73,7 @@ public class ServerMain {
 				if (TimeUntilMaint < TimeUntilOn && TimeUntilMaint < TimeUntilOff && TimeUntilMaint > 0) {
 					y.SetTimeProgress(100 - ((int) Math.round(100 * (TimeUntilMaint / TimeSchedule.TimeBetween(TimeSchedule.ServerOff, TimeSchedule.MaintTime)))), (int) Math.floor(TimeUntilMaint) + " hrs " + (int) Math.round((TimeUntilMaint - Math.floor(TimeUntilMaint)) * 60) + " min until Maintenance.");
 					EcoModeSegment = 3; } }
-			else y.SetTimeProgress(100, "∞ until Power Off");
+			else y.SetTimeProgress(0, "∞ until Power Off");
 			if (EcoModeSegment == 1) {
 				//do hibernation 1 min before server on.
 				//make sure maintenance stops before this code triggers.
@@ -76,7 +87,8 @@ public class ServerMain {
 				//a long sleep.
 				//do the maintenance here?
 			}
-			y.Output("Current Segment for Eco: " + EcoModeSegment);
+			try {y.Output("Current Segment for Eco: " + EcoModeSegment + "     CPU Usage: " + Math.round(CpuInfo.getCPU() * 100) + "%"); }
+			catch (ShellCommandUsageException e1) {e1.printStackTrace(); } catch (ShellCommandExecException e1) {e1.printStackTrace(); }
 			if (UpdateCountdown == 0) {
 				//TODO trigger update: check server status, cpu load/heat, players, network... AutoHotKey scripts...
 				UpdateCountdown = 600; }
