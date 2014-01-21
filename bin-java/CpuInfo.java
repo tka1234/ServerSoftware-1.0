01976dc1bfa13db41a57154264395c917779301b
T. Alexander <t.alex@outlook.com>
2014-01-20 21:06:26 -0500

    Update CpuInfo.java
    Massive cleanup. Found that combined works best.

--- bin-java/CpuInfo.java
+++ bin-java/CpuInfo.java
@@ -1,26 +1,10 @@
-import org.hyperic.sigar.CpuPerc;
-import org.hyperic.sigar.SigarException;
-import org.hyperic.sigar.cmd.Shell;
-import org.hyperic.sigar.cmd.SigarCommandBase;
-import org.hyperic.sigar.shell.ShellCommandExecException;
-import org.hyperic.sigar.shell.ShellCommandUsageException;
+import org.hyperic.sigar.SigarException; import org.hyperic.sigar.cmd.Shell; import org.hyperic.sigar.cmd.SigarCommandBase;
 public class CpuInfo extends SigarCommandBase {
 	public CpuInfo(Shell shell) {super(shell); }
 	public CpuInfo() {super(); }
-	private static double output(CpuPerc cpu) {
-		//println("User Time....." + CpuPerc.format(cpu.getUser()));
-		//println("Sys Time......" + CpuPerc.format(cpu.getSys()));
-		//println("Idle Time....." + CpuPerc.format(cpu.getIdle()));
-		//println("Wait Time....." + CpuPerc.format(cpu.getWait()));
-		//println("Nice Time....." + CpuPerc.format(cpu.getNice()));
-		//println("Combined......" + CpuPerc.format(cpu.getCombined()));
-		return cpu.getUser();
-		//println("Irq Time......" + CpuPerc.format(cpu.getIrq())); 
-		}
-	public void output(String[] args) throws SigarException {output(this.sigar.getCpuPerc()); }
-	
-	public static double getCPU() throws ShellCommandUsageException, ShellCommandExecException {
-		try {return output(new CpuInfo().sigar.getCpuPerc()); }
-		catch (SigarException e) {e.printStackTrace(); return 0; } }
-	
-	public static void main(String[] args) throws Exception {new CpuInfo().processCommand(args); } }
+	public static double getCPU() {
+		try {return new CpuInfo().sigar.getCpuPerc().getCombined(); }
+		catch (SigarException e) {e.printStackTrace(); return -1; } }
+	public static void main(String[] args) throws Exception {
+		while (true) {System.out.println(getCPU() * 100); } }
+	public void output(String[] arg0) throws SigarException { } }
